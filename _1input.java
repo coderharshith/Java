d64dad4471019df4f4ea27296ea1f8b37d86547b
coderharshith <130730951+coderharshith@users.noreply.github.com>
2024-04-29 22:57:23 +0530

    Add files via upload

---
+++ _1input.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class _1input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(n);
+    }
+}
