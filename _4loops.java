f5dbfc957cbd609949e11b626a765da84e4b9cf1
coderharshith <130730951+coderharshith@users.noreply.github.com>
2024-05-01 13:40:19 +0530

    Add files via upload

---
+++ _4loops.java
@@ -0,0 +1,31 @@
+public class _4loops {
+    public static void main(String[] args) {
+        // Loops in programming languages is a feature which facilitates the execution of a set of instructions/functions repeatedly while some condition evaluates to true.
+
+        // for loop
+        for(int i =1 ;i <=10;i++){
+            System.out.println(2+" * "+i+" = "+i*2);
+        }
+        System.out.println();
+
+        
+        // while loop
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(5+" * "+i+" = "+i*5);
+            i++;
+        }
+
+        System.out.println();
+
+
+        // do-while loop
+        // this still give the output once
+        i =11;
+        do{
+            System.out.println(8+" * "+i+" = "+i*8);
+            i++;
+        }
+        while(i <= 10);
+    }
+}
